0fb186d98f7aa557c5b5e4587a861dab837318b5
Vraneth <marcelopinov.23@gmail.com>
2025-06-12 21:28:50 -0400

    Inicio Aplicación

---
+++ src/main/java/org/example/yukiacademy/YukiAcademyApplication.java
@@ -0,0 +1,13 @@
+package org.example.yukiacademy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YukiAcademyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YukiAcademyApplication.class, args);
+    }
+
+}
